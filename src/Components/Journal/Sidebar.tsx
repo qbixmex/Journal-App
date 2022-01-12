0ef3c752ef263ab0fcac180c4edd282f0a7fe2da
Daniel <sonusbeat@gmail.com>
2022-01-12 16:18:47 -0600

    Sidebar Styles and Font Awesome

---
+++ src/Components/Journal/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUser, faCalendarPlus } from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <aside className="journal__sidebar">
+      <div className="journal__sidebar-navbar">
+
+        <p className="mt-3 mb-3">
+          <FontAwesomeIcon icon={ faUser } className="me-2" />
+          <span>John Doe</span>
+        </p>
+
+        <button className="btn">
+          logout
+        </button>
+      </div>
+
+      <div className="journal__new-entry">
+        <FontAwesomeIcon icon={ faCalendarPlus } size="5x" className="mb-4" />
+        <p>New Entry</p>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
