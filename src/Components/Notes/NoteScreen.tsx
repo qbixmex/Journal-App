4077c4485b83ecbeeb4b2f0fc624c4ffab22459c
Daniel <sonusbeat@gmail.com>
2022-01-12 22:37:11 -0600

    NotesScreen Styles Added

--- src/Components/Notes/NoteScreen.tsx
+++ src/Components/Notes/NoteScreen.tsx
@@ -1,9 +1,36 @@
 import NotesAppBar from "./NotesAppBar";
+import bokuNoHeroimage from "../../Assets/Images/boku-no-hero.jpg"
 
 const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+
+      <div className="notes__content">
+        <input
+          className="notes__content__title-input"
+          type="text"
+          placeholder="Subject Title"
+          autoComplete="off"
+          autoCorrect="off"
+        />
+
+        <textarea
+          id=""
+          name=""
+          className="notes__content__textarea"
+          autoComplete="off"
+          autoCorrect="off"
+          placeholder="What happened today ?"
+        ></textarea>
+
+        <div className="notes__content__image-box">
+          <img
+            src={ bokuNoHeroimage }
+            alt="Boku no hero"
+          />
+        </div>
+      </div>
     </div>
   );
 };
